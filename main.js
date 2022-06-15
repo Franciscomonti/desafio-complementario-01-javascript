221583c4f15a70dfd18d2773259d0786c74f074a
Francsico Monti <montifranciscojavier@gmail.com>
2022-06-15 16:57:47 -0300

    creacion de bucle for

---
+++ main.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("ingrese un numero para saber sus elevados"))
+
+
+for(let i= 1; i<=10; i++){
+
+    let resultado = numero**i;
+    console.log(`${numero} elevado por ${i}  = ${resultado}`)
+    }
